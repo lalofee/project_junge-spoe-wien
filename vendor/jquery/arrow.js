2a8e9cacc7d567e445a9c171a312fac8acfad7a5
lalofee <armstorfer@silverserver.at>
2018-10-04 15:05:57 +0200

    data

---
+++ vendor/jquery/arrow.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $(".one").hover(function(){
+        $("#one").css("font-size", "44px");
+        $("#one").css("-webkit-transform", "rotate(90deg)");
+        $("#one").css("-moz-transform", "rotate(90deg)");
+        $("#one").css("-o-transform", "rotate(90deg)");
+        $("#one").css("-ms-transform", "rotate(90deg)");
+        $("#one").css("transform", "rotate(90deg)");
+        }, function(){
+        $("#one").css("font-size", "36px");
+        $("#one").css("-webkit-transform", "rotate(0)");
+        $("#one").css("-moz-transform", "rotate(0)");
+        $("#one").css("-o-transform", "rotate(0)");
+        $("#one").css("-ms-transform", "rotate(0)");
+        $("#one").css("transform", "rotate(0)");
+    });
+});
+$(document).ready(function(){
+    $(".two").hover(function(){
+        $("#two").css("font-size", "44px");
+        $("#two").css("-webkit-transform", "rotate(90deg)");
+        $("#two").css("-moz-transform", "rotate(90deg)");
+        $("#two").css("-o-transform", "rotate(90deg)");
+        $("#two").css("-ms-transform", "rotate(90deg)");
+        $("#two").css("transform", "rotate(90deg)");
+        }, function(){
+          $("#two").css("font-size", "36px");
+         $("#two").css("-webkit-transform", "rotate(0)");
+        $("#two").css("-moz-transform", "rotate(0)");
+        $("#two").css("-o-transform", "rotate(0)");
+        $("#two").css("-ms-transform", "rotate(0)");
+        $("#two").css("transform", "rotate(0)");
+    });
+});
+$(document).ready(function(){
+    $(".three").hover(function(){
+        $("#three").css("font-size", "44px");
+        $("#three").css("-webkit-transform", "rotate(90deg)");
+        $("#three").css("-moz-transform", "rotate(90deg)");
+        $("#three").css("-o-transform", "rotate(90deg)");
+        $("#three").css("-ms-transform", "rotate(90deg)");
+        $("#three").css("transform", "rotate(90deg)");
+        }, function(){
+          $("#three").css("font-size", "36px");
+         $("#three").css("-webkit-transform", "rotate(0)");
+        $("#three").css("-moz-transform", "rotate(0)");
+        $("#three").css("-o-transform", "rotate(0)");
+        $("#three").css("-ms-transform", "rotate(0)");
+        $("#three").css("transform", "rotate(0)");
+    });
+});
